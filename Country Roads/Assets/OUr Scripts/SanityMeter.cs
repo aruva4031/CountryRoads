041a1e0e2d893068589f58dd5a1433a41869e079
JuliaHeleneW <jwerner@csumb.edu>
2018-10-16 16:57:48 -0700

    Added sanity event

--- Country Roads/Assets/OUr Scripts/SanityMeter.cs
+++ Country Roads/Assets/OUr Scripts/SanityMeter.cs
@@ -7,17 +7,30 @@ public class SanityMeter : MonoBehaviour {
     public int sanity = 100;
     public bool lowerSanity;
     public bool coroutine_running;
+    public GameObject bloodEffects;
 
     // Use this for initialization
     void Start () {
         lowerSanity = false;
         coroutine_running = false;
         InvokeRepeating("sanityLowering", 0.0f, 1.0f);
+        bloodEffects = GameObject.FindWithTag("BloodEffect");
+        bloodEffects.SetActive(false);
     }
 	
 	// Update is called once per frame
 	void Update () {
         GetComponent<UnityEngine.UI.Text>().text="Sanity: "+sanity;
+        if (sanity <= 45&&!coroutine_running)
+        {
+            StartCoroutine(sanityEvent());
+            coroutine_running = true;
+        }
+        else if (sanity > 45 && coroutine_running)
+        {
+            StopCoroutine(sanityEvent());
+            coroutine_running = false;
+        }
 	}
 
     public void sanityLowering()
@@ -38,9 +51,11 @@ public class SanityMeter : MonoBehaviour {
         lowerSanity = true;
     }
 
-    /*public IEnumerator sanityLoweringStart()
+    public IEnumerator sanityEvent()
     {
-        yield return new WaitForSeconds(15);
-        lowerSanity = true;
-    }*/
+        bloodEffects.SetActive(true);
+        yield return new WaitForSeconds(10);
+        bloodEffects.SetActive(false);
+        sanity += 10;
+    }
 }
