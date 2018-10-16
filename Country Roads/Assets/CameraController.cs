58322a86c51d01f7bae5bf953f9d23f9741b95f2
Jessica Ubaldo <jubaldo@csumb.edu>
2018-10-16 08:21:58 -0700

    added CameraController and Camera to Player

---
+++ Country Roads/Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        transform.rotation = player.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Input.GetAxis("HorizontalRS"));
+        if (Input.GetAxis("HorizontalRS").Equals())
+        {
+
+        }
+	}
+}
