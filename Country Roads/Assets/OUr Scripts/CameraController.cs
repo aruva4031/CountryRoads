6c69b37e762a1beb5dbec141c8f33bb2bd4cdbc9
Jessica Ubaldo <jubaldo@csumb.edu>
2018-11-11 16:25:26 -0800

    Camera rotates back to original view after a few seconds

--- Country Roads/Assets/OUr Scripts/CameraController.cs
+++ Country Roads/Assets/OUr Scripts/CameraController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
@@ -15,11 +14,18 @@ public class CameraController : MonoBehaviour
     public bool seeRadio;
 
     // is player dead
-    bool isKilled;
+    public bool isKilled;
+
+    // the official time
+    private float timeInSec;
+
+    // need initial and final time; when 5 seconds have passed since the initial time, do the wait then rotation
+    private int initalTime;
+    private int finalTime;
 
     // Use this for initialization
     void Start()
-    {  
+    {
         //this.player = GameObject.FindGameObjectWithTag("Player");
         this.radio = GameObject.FindGameObjectWithTag("Radio");
         this.ray = new Ray(transform.position, transform.forward);
@@ -29,8 +35,14 @@ public class CameraController : MonoBehaviour
         this.verticalRS = Input.GetAxis("VerticalRS");
         this.seeRadio = false;
         this.isKilled = false;
+
+        // initialize the seconds to 0
+        this.timeInSec = 0f;
+        this.initalTime = 0;
+        this.finalTime = 0;
     }
 
+    // code to see the radio
     public bool isRadioSeen()
     {
         RaycastHit hit;
@@ -50,76 +62,69 @@ public class CameraController : MonoBehaviour
         return this.seeRadio;
     }
 
-	public bool isGhostSeen(string tag) {
-		RaycastHit hit;
-		bool GhostSeen = false;
-		if (Physics.Raycast(new Ray(transform.position, transform.forward), out hit, 100f))
-		{
-			if (hit.collider.tag == tag)
-			{
-				GhostSeen = true;
-			}
-			else
-			{
-				GhostSeen = false;
-			}
-		}
-		return GhostSeen;
-	}
+    // Alex's code: to see the ghost
+    public bool isGhostSeen(string tag)
+    {
+        RaycastHit hit;
+        bool GhostSeen = false;
+        if (Physics.Raycast(new Ray(transform.position, transform.forward), out hit, 100f))
+        {
+            if (hit.collider.tag == tag)
+            {
+                GhostSeen = true;
+            }
+            else
+            {
+                GhostSeen = false;
+            }
+        }
+        return GhostSeen;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        // set the timeInSec here
+        this.timeInSec = timeInSec + Time.deltaTime;
+
+        Debug.Log("** Time is: " + timeInSec);
+
         horizontalRS = Input.GetAxis("HorizontalRS") * 90;
         verticalRS = Input.GetAxis("VerticalRS") * 45;
 
         look = Quaternion.Euler(transform.rotation.x + verticalRS, transform.rotation.y + horizontalRS, transform.rotation.z + 0);
-        if (GameObject.FindWithTag("StalkerGhost").GetComponent<StalkerGhostAI>().stopMovement==false)
+        if (GameObject.FindWithTag("StalkerGhost").GetComponent<StalkerGhostAI>().stopMovement == false)
         {
             // if the player is looking around
             if (this.horizontalRS >= 0.8 || this.horizontalRS <= -0.8 || this.verticalRS >= 0.8 || this.verticalRS <= -0.8)
             {
                 // if the player is not looking around
                 transform.rotation = Quaternion.Slerp(transform.rotation, look, Time.deltaTime * 3f);
+
+                // set the initial time by the seconds
+                this.timeInSec = this.timeInSec + Time.deltaTime;
+                this.initalTime = (int)(this.timeInSec);
+                // give finalTime the initalTime 5 seconds in advance
+                this.finalTime = this.initalTime + 5;
             }
             else
             {
-                // if the player is not looking around
-                transform.rotation = Quaternion.Slerp(transform.rotation, player.transform.rotation, Time.deltaTime * 3f);
+                this.initalTime = (int)(this.timeInSec);
+
+                // Debug.Log("## Initial Time is: " + this.initalTime);
+                // Debug.Log("## Final Time is: " + this.finalTime);
+
+                if (this.initalTime == this.finalTime)
+                {
+                    // if the player is not looking around
+                    transform.rotation = Quaternion.Slerp(transform.rotation, player.transform.rotation, Time.deltaTime * 3f);
+                }
             }
         }
         else
         {
+            // the Stalker ghost has stopped moving
             transform.rotation = Quaternion.Slerp(transform.rotation, player.transform.rotation, Time.deltaTime * 3f);
         }
-
-
-        //TODO: fixupdate method
-        /*
-        if (this.horizontalRS >= 0.8)
-        {
-            this.transform.Rotate(Vector3.down * Time.deltaTime * -60);
-            Debug.Log("Go into horizontalRS if");
-        }
-        else if (this.horizontalRS <= -0.8)
-        {
-            this.transform.Rotate(Vector3.down * Time.deltaTime * 60);
-        }
-        else if (this.horizontalRS < 0.8 || this.horizontalRS > -0.8)
-        {
-            if (this.horizontalRS > 0.15)
-            {
-                this.transform.Rotate(Vector3.down * Time.deltaTime * -15);
-            }
-            else if (this.horizontalRS < -0.15)
-            {
-                this.transform.Rotate(Vector3.down * Time.deltaTime * 15);
-            }
-            else
-            {
-               // rotate the camera back to the original position
-               this.transform.rotation = Quaternion.Lerp(this.transform.rotation, player.transform.rotation, Time.time * lerpValue);
-            }
-        }*/
     }
 }
