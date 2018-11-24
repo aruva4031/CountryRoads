94b54d380d8cb2d6df82efb0ef4f6c64ec96bbb0
jubaldo1 <jubaldo@csumb.edu>
2018-11-24 14:32:52 -0800

    menu

---
+++ Country Roads/Assets/OUr Scripts/SceneSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : MonoBehaviour {
+    /*
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}*/
+
+    public void LoadScene(int sceneNum)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Entered QuitGame");
+        Application.Quit();
+    }
+}
