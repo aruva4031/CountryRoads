555423bcc489ba63a1f5fffd605c4364b605f2ff
Jessica May Ubaldo <ubal8146@csumb.ad.csumb.edu>
2018-12-08 14:33:48 -0800

    Start Scene

---
+++ Country Roads/Assets/DifficultyOptions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyOptions : MonoBehaviour {
+    public Slider difficultySlider;
+
+	// Use this for initialization
+	void Start () {
+		difficultySlider.onValueChanged.AddListener(delegate { DifficultyChanged(); });
+	}
+	/*
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    */
+
+    // take in difficulty change
+    void DifficultyChanged()
+    {
+        Debug.Log(difficultySlider.value);
+    }
+}
