4195bb7668a9831750ce060006fa92b248639025
Jessica Ubaldo <jubaldo@csumb.edu>
2018-12-08 12:33:33 -0800

    start scene changes

---
+++ Country Roads/Assets/DisplayOptions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayOptions : MonoBehaviour {
+
+    public CanvasGroup startOptions;
+    public CanvasGroup displayOptions;
+
+	// Use this for initialization
+	void Start () {
+        //this.startOptions;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
