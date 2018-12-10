4ab9ee25ce33ec8bc1dbde9dcbe4643df0fad3f4
aruva4031 <aruvalcaba@csumb.edu>
2018-12-10 14:46:08 -0800

    added the road collision detector script

---
+++ Country Roads/Assets/OUr Scripts/RoadCollisionDetector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadCollisionDetector : MonoBehaviour {
+
+
+	public bool hitARoad = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if(other.tag == "straightRoad" || other.tag == "curvedroad" || other.tag == "curvedroadleft")
+		{
+			hitARoad = true;
+		}
+	}
+}
